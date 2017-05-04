80678c6e3cf79b7296a2e06edaa3152b17bc47b1
timour.meeusen <timour.meeusen@B209-11>
2017-05-04 11:22:37 +0200

    Add project files.

---
+++ BotsBalGame/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace BotsbalGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictBoxBal = new System.Windows.Forms.PictureBox();
+            this.pictBoxCPU = new System.Windows.Forms.PictureBox();
+            this.pictBoxSpeler = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panelSpeelveld = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxBal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxSpeler)).BeginInit();
+            this.panelSpeelveld.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictBoxBal
+            // 
+            this.pictBoxBal.BackColor = System.Drawing.Color.Red;
+            this.pictBoxBal.Location = new System.Drawing.Point(306, 59);
+            this.pictBoxBal.Name = "pictBoxBal";
+            this.pictBoxBal.Size = new System.Drawing.Size(30, 30);
+            this.pictBoxBal.TabIndex = 0;
+            this.pictBoxBal.TabStop = false;
+            // 
+            // pictBoxCPU
+            // 
+            this.pictBoxCPU.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictBoxCPU.Location = new System.Drawing.Point(10, 59);
+            this.pictBoxCPU.Name = "pictBoxCPU";
+            this.pictBoxCPU.Size = new System.Drawing.Size(10, 100);
+            this.pictBoxCPU.TabIndex = 1;
+            this.pictBoxCPU.TabStop = false;
+            // 
+            // pictBoxSpeler
+            // 
+            this.pictBoxSpeler.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictBoxSpeler.Location = new System.Drawing.Point(706, 59);
+            this.pictBoxSpeler.Name = "pictBoxSpeler";
+            this.pictBoxSpeler.Size = new System.Drawing.Size(10, 100);
+            this.pictBoxSpeler.TabIndex = 2;
+            this.pictBoxSpeler.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // panelSpeelveld
+            // 
+            this.panelSpeelveld.Controls.Add(this.lblScore);
+            this.panelSpeelveld.Controls.Add(this.pictBoxSpeler);
+            this.panelSpeelveld.Controls.Add(this.pictBoxCPU);
+            this.panelSpeelveld.Controls.Add(this.pictBoxBal);
+            this.panelSpeelveld.Location = new System.Drawing.Point(1, 0);
+            this.panelSpeelveld.Name = "panelSpeelveld";
+            this.panelSpeelveld.Size = new System.Drawing.Size(731, 390);
+            this.panelSpeelveld.TabIndex = 3;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(303, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(33, 13);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "0 - 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 389);
+            this.Controls.Add(this.panelSpeelveld);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxBal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxSpeler)).EndInit();
+            this.panelSpeelveld.ResumeLayout(false);
+            this.panelSpeelveld.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictBoxBal;
+        private System.Windows.Forms.PictureBox pictBoxCPU;
+        private System.Windows.Forms.PictureBox pictBoxSpeler;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Panel panelSpeelveld;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
---
+++ BotsBalGame/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace BotsbalGame
+{
+    public partial class Form1 : Form
+    {
+        private int snelheidLinks = 10;
+        private int snelheidTop = 10;
+
+        private int scoreCPU = 0;
+        private int scoreSpeler = 0;
+
+        private Random rndRGB = new Random();
+        private Random rndLatenWinnen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Start();
+            Cursor.Hide();
+            FormBorderStyle = FormBorderStyle.None; //geen "boders", grenzen, randen (zoals bv. 'snelmenu')
+            TopMost = true; //frm naar voorgrond
+            Bounds = Screen.PrimaryScreen.Bounds; //fullscreen
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //cursor laat pictBoxSpeler bewegen (op en neer)
+            pictBoxSpeler.Top = Cursor.Position.Y / 2;
+
+            //bal laten bewegen en weerkaatsen als het de top/bodem raakt
+            pictBoxBal.Left += snelheidLinks;
+            pictBoxBal.Top += snelheidTop;
+
+            if (pictBoxBal.Top <= panelSpeelveld.Top || pictBoxBal.Bottom >= panelSpeelveld.Bottom)
+            {
+                snelheidTop = -snelheidTop;
+            }
+
+            //AI (= CPU) laten meespelen
+            if (rndLatenWinnen.Next(50) < 25)
+            {
+                while (pictBoxBal.Top < pictBoxCPU.Top)
+                {
+                    pictBoxCPU.Top--;
+                }
+
+                while (pictBoxBal.Bottom > pictBoxCPU.Bottom)
+                {
+                    pictBoxCPU.Top++;
+                }
+            }
+
+            //bal terugkaatsen indien ie een pictBox raakt en evt. (bij < 20) versnellen
+            if (pictBoxBal.Right >= pictBoxSpeler.Left && pictBoxBal.Top >= pictBoxSpeler.Top && pictBoxBal.Bottom <= pictBoxSpeler.Bottom
+               || pictBoxBal.Left <= pictBoxCPU.Right && pictBoxBal.Top >= pictBoxCPU.Top && pictBoxBal.Bottom <= pictBoxCPU.Bottom)
+            {
+                snelheidLinks = -snelheidLinks;
+
+                if (snelheidLinks < 20)
+                {
+                    snelheidLinks++;
+                }
+            }
+
+            //als pictBoxBal uit panelSpeelveld is, moet er een pt worden bijgeteld en de bal opnieuw verschijnen in panelSpeelveld (in het midden)
+            //en snelheidLinks wordt gereset
+            if (pictBoxBal.Right >= panelSpeelveld.Right)
+            {
+                scoreCPU++;
+                panelSpeelveld.BackColor = Color.FromArgb(rndRGB.Next(0, 255), rndRGB.Next(10, 255), rndRGB.Next(10, 255));
+                snelheidLinks = 10;
+                pictBoxBal.Top = panelSpeelveld.Bottom / 2;
+                pictBoxBal.Left = panelSpeelveld.Right / 2;
+            }
+
+            if (pictBoxBal.Left <= panelSpeelveld.Left)
+            {
+                scoreSpeler++;
+                panelSpeelveld.BackColor = Color.FromArgb(rndRGB.Next(0, 255), rndRGB.Next(10, 255), rndRGB.Next(10, 255));
+                snelheidLinks = 10;
+                pictBoxBal.Top = panelSpeelveld.Bottom / 2;
+                pictBoxBal.Left = panelSpeelveld.Right / 2;
+            }
+
+            //geüpdatete score verschijnt op het scherm
+            lblScore.Text = scoreCPU + " - " + scoreSpeler;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Esc om af te sluiten (de uitslag verschijnt nog)
+            if (e.KeyCode == Keys.Escape)
+            {
+                if (scoreSpeler > scoreCPU)
+                {
+                    MessageBox.Show("U heeft gewonnen: " + lblScore.Text);
+                }
+                else if (scoreCPU > scoreSpeler)
+                {
+                    MessageBox.Show("Game over: " + lblScore.Text);
+                }
+                else
+                {
+                    MessageBox.Show("beiden even sterk/zwak: " + lblScore.Text);
+                }
+
+                Close();
+            }
+
+            ////pijltje naar beneden om pictBoxSpeler naar beneden te verplaatsen
+            //if (e.KeyCode == Keys.Down)
+            //{
+            //    pictBoxSpeler.Top += 40;
+            //}
+
+
+            ////pijltje naar boven om pictBoxSpeler naar boven te verplaatsen
+            //if (e.KeyCode == Keys.Up)
+            //{
+            //    pictBoxSpeler.Top -= 40;
+        }
+    }
+}
